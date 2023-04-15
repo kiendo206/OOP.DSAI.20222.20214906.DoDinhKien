45bf155231cfc4f11de7da1e8bab9ec1d6b4a22e
kiendo206 <kiendo206@gmail.com>
2023-04-15 16:06:22 +0700

    Exercise 2.2.5

---
+++ Lab01/Exercise_2.2.5/src/Calculator.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+public class Calculator {
+
+	public static void main(String[] args) {
+		// Input the numbers
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input the first number: ");
+		double num1 = sc.nextDouble();
+		System.out.print("Input the second number: ");
+		double num2 = sc.nextDouble();
+		// Calculate sum, difference, produce and quotient
+		System.out.printf("Sum of two numbers: %f%n", num1 + num2);
+		System.out.printf("Difference of two numbers: %f%n", num1 - num2);
+		System.out.printf("Product of two numbers: %f%n", num1*num2);
+		if (num2==0) {
+			System.out.print("Quotient does not exist");
+		}
+		else
+		{
+			System.out.printf("Quotient of two numbers: %f%n", num1/num2);
+		}
+	}
+
+}
