fc45d84ccfa482d50c4e20da48d779ff3536a6dc
kiendo206 <kiendo206@gmail.com>
2023-05-15 21:59:35 +0700

    refactor

--- Lab03/AimsProject/src/hust/soict/dsai/test/store/StoreTest/StoreTest.java
+++ Lab03/AimsProject/src/hust/soict/dsai/test/store/StoreTest/StoreTest.java
@@ -1,20 +1,21 @@
 package hust.soict.dsai.test.store.StoreTest;
+
 import hust.soict.dsai.aims.disc.DigitalVideoDisc.DigitalVideoDisc;
 import hust.soict.dsai.aims.store.Store.Store;
 
 public class StoreTest {
-    public static void main(String[] args) {
-        Store store = new Store();
+	public static void main(String[] args) {
+		Store store = new Store();
 
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Kemtrinh");
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("Kemtrinh");
 		store.addDVD(dvd1);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("OOOOOOOO");
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("OOOOOOOO");
 		store.addDVD(dvd2);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Ohmychuoi");
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Ohmychuoi");
 		store.addDVD(dvd3);
 
-        store.removeDVD(dvd2);
-        store.removeDVD(dvd1);
-        store.removeDVD(dvd3);
-    }
+		store.removeDVD(dvd2);
+		store.removeDVD(dvd1);
+		store.removeDVD(dvd3);
+	}
 }
