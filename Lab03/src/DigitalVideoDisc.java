7c88ba1c4f695e5485592a2fca755194b4d45222
kiendo206 <kiendo206@gmail.com>
2023-05-15 19:43:16 +0700

    Class member

--- Lab03/src/DigitalVideoDisc.java
+++ Lab03/src/DigitalVideoDisc.java
@@ -1,14 +1,24 @@
 
 public class DigitalVideoDisc {
+	private String title;
+	private String category;
+	private String director;
+	private int length;
+	private float cost;
+	private int id;
+	private static int nbDigitalVideoDiscs = 0;
+	
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
+		this.id = ++nbDigitalVideoDiscs; 
 	}
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
+		this.id = ++nbDigitalVideoDiscs; 
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
@@ -16,6 +26,7 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.director = director;
 		this.cost = cost;
+		this.id = ++nbDigitalVideoDiscs; 
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
@@ -24,12 +35,9 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+		this.id = ++nbDigitalVideoDiscs; 
 	}
-	private String title;
-	private String category;
-	private String director;
-	private int length;
-	private float cost;
+
 	public String getTitle() {
 		return title;
 	}
@@ -45,4 +53,15 @@ public class DigitalVideoDisc {
 	public float getCost() {
 		return cost;
 	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getId() {
+		return id;
+	}
+	public static int getNbDigitalVideoDiscs() {
+		return nbDigitalVideoDiscs;
+	}
+	
+	
 }
