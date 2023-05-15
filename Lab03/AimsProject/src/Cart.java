ca1609946529cfdcc7b6c313bc8563d841c742dc
kiendo206 <kiendo206@gmail.com>
2023-05-15 20:56:39 +0700

    Store

--- Lab03/AimsProject/src/Cart.java
+++ Lab03/AimsProject/src/Cart.java
@@ -4,7 +4,6 @@ public class Cart {
 	public static final int MAX_NUMBERS_ORDERED=20;
 	private DigitalVideoDisc itemsOrdered[]=
 			new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
-	private int qtyOrdered = 0;
 	
 	public Cart() {
 		super();
